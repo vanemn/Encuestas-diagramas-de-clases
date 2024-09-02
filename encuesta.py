acc2d82a786ab8f7dc41e281f29aa249fdcd7716
Nicole Pinilla <npinilla89@gmail.com>
2024-09-02 19:17:06 -0400

    se agregan clases a archivo encuesta por edad y region

--- encuesta.py
+++ encuesta.py
@@ -1,4 +1,5 @@
 from pregunta import Pregunta
+from listado_respuestas import ListadoRespuestas
 
 
 class Encuesta:
@@ -9,3 +10,24 @@ class Encuesta:
 
     def agregarPregunta(self, pregunta: Pregunta):
         self._preguntas.append(pregunta)
+
+    def agregarListadoRespuestas(self, listado_respuestas):
+        self._listadosRespuestas.append(listado_respuestas)
+
+    def mostrarEncuesta(self):
+        print(f"Nombre: {self.nombre}")
+        for pregunta in self._preguntas:
+            pregunta.mostrarPregunta()
+
+
+class EncuestaLimitadaEdad(Encuesta):
+    def __init__(self, nombre: str, edadMinima: int, edadMaxima: int):
+        super().__init__(nombre)
+        self.edadMinima = edadMinima
+        self.edadMaxima = edadMaxima
+
+
+class EncuestaLimitadaRegion(Encuesta):
+    def __init__(self, nombre: str, regiones: list[int]):
+        super().__init__(nombre)
+        self.regiones = regiones
