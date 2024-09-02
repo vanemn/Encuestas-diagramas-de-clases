2e4cc1bea0cd529901e94dd66c308e85d729ac68
Nicole Pinilla <npinilla89@gmail.com>
2024-09-02 19:30:43 -0400

    Se agregan docstrings

--- encuesta.py
+++ encuesta.py
@@ -3,31 +3,91 @@ from listado_respuestas import ListadoRespuestas
 
 
 class Encuesta:
+    """
+    Clase que representa una encuesta.
+
+    Métodos:
+        agregarPregunta(pregunta: Pregunta): Agrega una pregunta a la encuesta.
+        agregarListadoRespuestas(listado_respuestas): Agrega un listado de respuestas a la encuesta.
+        mostrarEncuesta(): Muestra el nombre de la encuesta y las preguntas asociadas.
+    """
+
     def __init__(self, nombre: str):
+        """
+        Metodo constructor que Inicializa una nueva instancia de Encuesta.
+
+        Args:
+            nombre (str): El nombre de la encuesta.
+
+        Atributos:
+        nombre (str): El nombre de la encuesta.
+        _preguntas (list): Lista de preguntas que forman parte de la encuesta.
+        _listadosRespuestas (list): Lista de listados de respuestas asociadas a la encuesta.
+        """
         self.nombre = nombre
         self._preguntas = []
         self._listadosRespuestas = []
 
     def agregarPregunta(self, pregunta: Pregunta):
+        """
+        Agrega una pregunta a la encuesta.
+
+        Args:
+            pregunta (Pregunta): La pregunta que se desea agregar.
+        """
         self._preguntas.append(pregunta)
 
     def agregarListadoRespuestas(self, listado_respuestas):
+        """
+        Agrega un listado de respuestas a la encuesta.
+
+        Args:
+            listado_respuestas (ListadoRespuestas): El listado de respuestas que se desea agregar.
+        """
         self._listadosRespuestas.append(listado_respuestas)
 
     def mostrarEncuesta(self):
+        """
+        Muestra el nombre de la encuesta y las preguntas asociadas.
+        """
         print(f"Nombre: {self.nombre}")
         for pregunta in self._preguntas:
             pregunta.mostrarPregunta()
 
 
 class EncuestaLimitadaEdad(Encuesta):
+    """
+    Clase que representa una encuesta limitada por un rango de edad.
+
+    """
+
     def __init__(self, nombre: str, edadMinima: int, edadMaxima: int):
+        """
+        Inicializa una nueva instancia de EncuestaLimitadaEdad.
+
+        Args:
+            nombre (str): El nombre de la encuesta.
+            edadMinima (int): La edad mínima requerida para participar.
+            edadMaxima (int): La edad máxima permitida para participar.
+        """
         super().__init__(nombre)
         self.edadMinima = edadMinima
         self.edadMaxima = edadMaxima
 
 
 class EncuestaLimitadaRegion(Encuesta):
+    """
+    Clase que representa una encuesta limitada por regiones.
+
+    """
+
     def __init__(self, nombre: str, regiones: list[int]):
+        """
+        Inicializa una nueva instancia de EncuestaLimitadaRegion.
+
+        Args:
+            nombre (str): El nombre de la encuesta.
+            regiones (list[int]): Lista de identificadores de las regiones permitidas para participar.
+        """
         super().__init__(nombre)
         self.regiones = regiones
